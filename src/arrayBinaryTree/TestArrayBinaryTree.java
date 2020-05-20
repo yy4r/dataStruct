ff259468a4b78b359418a79d19c940e7eb861e85
Jack <yy4r@github.com>
2020-05-20 15:07:34 +0800

    栈和节点

---
+++ src/arrayBinaryTree/TestArrayBinaryTree.java
@@ -0,0 +1,12 @@
+package arrayBinaryTree;
+
+public class TestArrayBinaryTree {
+
+	public static void main(String[] args) {
+		int[] data = new int[] {1,2,3,4,5,6,7};
+		ArrayBinaryTree tree = new ArrayBinaryTree(data);
+		//前序遍历
+		tree.frontShow();
+	}
+
+}
